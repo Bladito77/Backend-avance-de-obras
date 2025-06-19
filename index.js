225eeb50ea6898d3c3980e2d9892b8b081f44459
bladito77 <edblatoco@gmail.com>
2025-06-18 22:21:06 -0500

    fix: dotenv cargado antes que sequelize

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require('./app'); 
 require('dotenv').config();
+const app = require('./app'); 
 const { sequelize } = require('./models');
 
 const PORT = process.env.PORT || 3000;
@@ -8,7 +8,7 @@ sequelize.authenticate()
   .then(() => {
     console.log('Conexión establecida con la base de datos');
     app.listen(PORT, () => {
-      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+      console.log(`Servidor corriendo en http://dominio:${PORT}`);
     });
   })
   .catch(err => console.error('Error al conectar con la base de datos:', err));
