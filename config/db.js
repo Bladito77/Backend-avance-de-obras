a0bbdaaac64e6d2daa2247a4a12873d4d3d49b54
bladito77 <edblatoco@gmail.com>
2025-06-21 21:52:05 -0500

    🚀 ultima vez2 que vaca2

--- config/db.js
+++ config/db.js
@@ -16,6 +16,11 @@
 //   }
 // );
 require('dotenv').config();
+console.log("🚀 Variables cargadas:");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
@@ -24,7 +29,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
     // dialectOptions: {
