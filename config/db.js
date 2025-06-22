ed204ca772ebf1701a59c6dc2bb5971ac9238083
bladito77 <edblatoco@gmail.com>
2025-06-21 21:07:36 -0500

    🚀 mas maricadas por Dios

--- config/db.js
+++ config/db.js
@@ -15,21 +15,42 @@
 //     logging: false
 //   }
 // );
-
-// module.exports = sequelize;
-const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      }
     }
   }
-});
+);
 
 module.exports = sequelize;
+
+// module.exports = sequelize;
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'mysql',
+//   logging: false,
+//   dialectOptions: {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     }
+//   }
+// });
+
+// module.exports = sequelize;
 
 
